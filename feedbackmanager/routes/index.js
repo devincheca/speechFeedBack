ec36adb6ab8ca096403d5fad5addfcb04befdd98
Devin Checa <devincheca@gmail.com>
2020-11-23 20:48:03 -0800

    handlebars is ideal for this

--- feedbackmanager/routes/index.js
+++ feedbackmanager/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/getLink', (req, res, next) => {
+  res.json({ isSuccess: true });
+});
+
 module.exports = router;
