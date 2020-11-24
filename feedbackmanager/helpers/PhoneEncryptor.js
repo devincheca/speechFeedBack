7219a24d2b5535a221406e7e61278e3725506666
Devin Checa <devincheca@gmail.com>
2020-11-23 21:18:49 -0800

    going to just keep this simple with dynamo

--- feedbackmanager/helpers/PhoneEncryptor.js
+++ feedbackmanager/helpers/PhoneEncryptor.js
@@ -1,8 +1,24 @@
+const crypto = require('crypto');
+const algorithm = 'aes-192-cbc';
+const password = '6826691700';
+
 export default class PhoneEncryptor {
   constructor() {
+    this.phoneNumber = '';
     this.encryptor.encryptedNumber = '';
   }
   encryptNumber() {
-
+    crypto.scrypt(password, 'salt', 24, (err, key) => {
+      if (err) throw err;
+      crypto.randomFill(new Uint8Array(16), (err, iv) => {
+        if (err) throw err;
+        const cipher = crypto.createCipheriv(algorithm, key, iv);
+        let encrypted = cipher.update(this.phoneNumber, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+        // save to DB here
+        return encrypted;
+      });
+    });
   }
 }
+
