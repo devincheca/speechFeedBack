3b102f24fb140c56c662217cc907775d1ba2e930
Devin Checa <devincheca@gmail.com>
2020-11-27 21:01:07 -0800

    loader works, now to do the mobile and copy paste button

--- feedbackmanager/public/hbsJs/index.js
+++ feedbackmanager/public/hbsJs/index.js
@@ -3,10 +3,23 @@ async function getLink() {
   if (!this.state.phoneNumber) {
     return errorMessage('Input a phone number above to receive feedback');
   }
+  toggleLoaderButton();
   const res = await req({
     data: { phoneNumber: this.state.phoneNumber },
     endpoint: '/getLink'
   });
   document.getElementById('feedbackButtonDiv').style.display = 'none';
   document.getElementById('linkDiv').innerHTML = window.localStorage.domain + '/feedback/' + res.token;
+  toggleLoaderButton();
+}
+function toggleLoaderButton() {
+  const feedbackButton = document.getElementById('feedbackButton');
+  const loaderButton = document.getElementById('loaderButton');
+  if (loaderButton.style.display === 'none') {
+    feedbackButton.style.display = 'none';
+    loaderButton.style.display = 'initial';
+  } else {
+    feedbackButton.style.display = 'initial';
+    loaderButton.style.display = 'none';
+  }
 }
