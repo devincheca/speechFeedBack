610cf13f30367cbd8ccd5445d474d1af0cfc48c8
Devin Checa <devincheca@gmail.com>
2020-11-27 21:27:31 -0800

    almost all the copy paste stuff is here, the last thing will be the mobile stuff

--- feedbackmanager/public/hbsJs/index.js
+++ feedbackmanager/public/hbsJs/index.js
@@ -8,9 +8,13 @@ async function getLink() {
     data: { phoneNumber: this.state.phoneNumber },
     endpoint: '/getLink'
   });
-  document.getElementById('feedbackButtonDiv').style.display = 'none';
-  document.getElementById('linkDiv').innerHTML = window.localStorage.domain + '/feedback/' + res.token;
   toggleLoaderButton();
+  document.getElementById('feedbackButtonDiv').style.display = 'none';
+  document.getElementById('linkDiv').style.display = 'initial';
+  document.getElementById('copyButtonDiv').style.display = 'initial';
+  const linkInput = document.getElementById('linkInput');
+  linkInput.value = window.localStorage.domain + '/feedback/' + res.token;
+  copyToClipboard(linkInput.value);
 }
 function toggleLoaderButton() {
   const feedbackButton = document.getElementById('feedbackButton');
@@ -22,4 +26,29 @@ function toggleLoaderButton() {
     feedbackButton.style.display = 'initial';
     loaderButton.style.display = 'none';
   }
+}
+function copyToClipboard(text) {
+  if (window.clipboardData && window.clipboardData.setData) {
+    return clipboardData.setData("Text", text);
+  }
+  else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+    const textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+    }
+    catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    }
+    finally {
+      document.body.removeChild(textarea);
+    }
+  }
+  const copyStatus = document.getElementById('copyStatus');
+  copyStatus.innerHTML = 'Copied!';
+  setTimeout(() => { copyStatus.innerHTML = ''; }, 4000);
 }
