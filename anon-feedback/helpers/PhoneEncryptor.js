c795ee984812bd097c7ba0abb6c66ed1ecfcf896
Devin Checa <devincheca@gmail.com>
2021-01-07 20:12:32 -0800

    holy cow this would be cool if the environments link right

--- anon-feedback/helpers/PhoneEncryptor.js
+++ anon-feedback/helpers/PhoneEncryptor.js
@@ -1,7 +1,6 @@
-const AWS = require('aws-sdk');
-AWS.config.update({region:'us-east-1'});
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
+const faunadb = require('faunadb'),
+  q = faunadb.query
+const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET })
 const crypto = require('crypto');
 const algorithm = 'aes-192-cbc';
 const password = '6826691700';
@@ -23,15 +22,20 @@ class PhoneEncryptor {
           encrypted += cipher.final('hex');
           const params = {
             Item: {
-              'tm-anon-links_id': { S: encrypted },
-              'key': { B: key },
-              'iv': { B: Buffer.from(iv) },
-              'phoneNumber': { S: encrypted },
-              'timeStamp': { N: (Math.floor(Date.now() / 1000) + (24*60*60)).toString() }
+              'tm-anon-links_id': encrypted,
+              'key': key,
+              'iv': Buffer.from(iv),
+              'phoneNumber': encrypted,
+              'timeStamp': (Math.floor(Date.now() / 1000) + (24*60*60)).toString()
             },
             TableName: 'tm-anon-links'
           };
-          await dynamodb.putItem(params).promise();
+          const createP = await client.query(
+            q.Create(
+              q.Collection(params.TableName),
+              { data: params.Item }
+            )
+          );
           this.callback(encrypted);
           this.encryptedNumber = encrypted;
         });
