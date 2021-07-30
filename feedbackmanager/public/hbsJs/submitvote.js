f3698ff54242ad3762a35568dc4f9e717be562e2
Devin Checa <devincheca@gmail.com>
2021-07-29 20:21:15 -0700

    this works

--- feedbackmanager/public/hbsJs/submitvote.js
+++ feedbackmanager/public/hbsJs/submitvote.js
@@ -3,19 +3,59 @@ async function castVote() {
   if (!this.state.vote) {
     return errorMessage('Please enter someone to vote for');
   }
+  if (isAlreadyVoted()) {
+    return errorMessage('You cannot vote twice.');
+  }
   toggleLoaderButton();
+  const linkToken = getToken();
   const res = await req({
     data: {
       vote: this.state.vote,
-      link: getToken()
+      link: linkToken
     },
     endpoint: '/saveVote'
   });
   toggleLoaderButton();
   if (res.isSaved) {
     successMessage('Your vote was successfully cast');
+    setCastVote();
   } else {
     errorMessage('Oops. Something went wrong. Refresh the page and try again.');
+  }
+}
+function setCastVote() {
+  try {
+    const linkToken = getToken();
+    const { votesCast } = localStorage;
+    const votesCastArray = votesCast ? JSON.parse(votesCast) : [];
+    if (votesCastArray) {
+      if (Array.isArray(votesCastArray)) {
+        votesCastArray.push(linkToken);
+        localStorage.votesCast = JSON.stringify(votesCastArray);
+      } else {
+        localStorage.votesCast = JSON.stringify([linkToken]);
+      }
+    }
+  } catch(error) { console.trace(error); }
+}
+function isAlreadyVoted() {
+  try {
+    const linkToken = getToken();
+    const { votesCast } = localStorage;
+    if (!votesCast) {
+      return false;
+    }
+    const votesCastArray = JSON.parse(votesCast);
+    if (votesCastArray) {
+      return Array.isArray(votesCastArray)
+        ? votesCastArray.includes(linkToken)
+        : votesCastArray.toString() === linkToken;
+    } else {
+      return false;
+    }
+  } catch(error) {
+    console.trace(error);
+    return false;
   }
 }
 function toggleLoaderButton() {
