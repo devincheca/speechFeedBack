ca21e31c19876e2b76b7c6cff80f439341ebff94
Devin Checa <devincheca@gmail.com>
2021-08-21 12:02:24 -0700

    this has all of the necessary pieces I would need to design something nice, I just need to really think of what is a good design

---
+++ feedbackmanager/public/hbsJs/homepage.js
@@ -0,0 +1,6 @@
+var state = {};
+window.onload = () => {
+  console.log('start timeout here');
+  // TODO: implement a typing animation to show and explain what each one is for
+  // with a slide down panel sort of design
+};
