be348406eb532196464ad8da865f5e1b53713745
Devin Checa <devincheca@gmail.com>
2022-02-12 11:20:46 -0800

    next is to add ngrx

--- feedbackmanager/feedback-angular/karma.conf.js
+++ feedbackmanager/feedback-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/feedback-ui'),
+      dir: require('path').join(__dirname, './coverage/feedback-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
