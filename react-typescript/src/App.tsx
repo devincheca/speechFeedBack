b708eba1466f91d2b8e0f39c0445f56ab07e9faf
Devin Checa <devincheca@gmail.com>
2022-04-07 21:13:21 -0700

    home page works, next step is to wire the other two pages

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,58 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import {
-  Banner,
-  FeedbackBullets,
-  VoteBullets,
+  Home
 } from './containers';
 
 function App() {
-  const [isFeedbackShowing, setFeedbackShowing] = useState(true);
-
-  const showFeedbackPage = () => setFeedbackShowing(!isFeedbackShowing);
-
-  const getFeedbackBtnClass = () => isFeedbackShowing ? "btn btn-primary nav-link margin-25" : "btn btn-secondary nav-link margin-25";
-
-  const getVotingBtnClass = () => !isFeedbackShowing ? "btn btn-primary nav-link margin-25" : "btn btn-secondary nav-link margin-25";
-
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-header-div">
-          <Banner></Banner>
-          <ul className="nav">
-            <li className="nav-item">
-              <button className={getFeedbackBtnClass()} style={{ cursor: 'pointer' }} onClick={() => showFeedbackPage()} id="feedbackNavLink">Feedback</button>
-            </li>
-            <li className="nav-item">
-              <button className={getVotingBtnClass()} style={{ cursor: 'pointer' }} onClick={() => showFeedbackPage()} id="voteNavLink">Voting</button>
-            </li>
-          </ul>
-          { isFeedbackShowing &&
-            <div id="feedbackDiv">
-              <FeedbackBullets></FeedbackBullets>
-              <div className="text-right">
-                <a href="/feedback">
-                  <button type="button" className="btn btn-primary" id="feedbackButton">Feedback System</button>
-                </a>
-              </div>
-              <div className="vertical-margin text-right">
-                <a href="/feedback?isTutorial=true">
-                  <button type="button" className="btn btn-secondary" id="feedbackButton">Feedback Tutorial</button>
-                </a>
-              </div>
-            </div>
-          }
-          { !isFeedbackShowing &&
-            <div id="voteDiv" className="vertical-margin">
-              <VoteBullets></VoteBullets>
-              <div className="text-right">
-                <a href="/vote">
-                  <button type="button" className="btn btn-primary" id="feedbackButton">Voting System</button>
-                </a>
-              </div>
-            </div>
-          }
+          <Home />
         </div>
       </header>
     </div>
--- react-typescript/src/containers/index.tsx
+++ react-typescript/src/containers/index.tsx
@@ -1,9 +1,11 @@
 import Banner from './Banner';
 import FeedbackBullets from './FeedbackBullets';
+import Home from './Home';
 import VoteBullets from './VoteBullets';
 
 export {
   Banner,
   FeedbackBullets,
+  Home,
   VoteBullets,
 };
